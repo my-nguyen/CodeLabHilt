56f7513c04f5952d8ab7ee67bd297661eaed22dd
Mỹ Nguyễn <nguyen@MacBook-Pro-M1Max-32G-1G.local>
2024-02-28 14:10:51 -0800

    UI Testing

---
+++ app/src/androidTest/java/com/nguyen/codelabhilt/AppTest.kt
@@ -0,0 +1,50 @@
+package com.nguyen.codelabhilt
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
+import com.nguyen.codelabhilt.ui.MainActivity
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.hamcrest.Matchers.containsString
+import org.junit.After
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@HiltAndroidTest
+class AppTest {
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
+    @After
+    fun tearDown() {
+        // Remove logs after the test finishes
+        ServiceLocator(getInstrumentation().targetContext).loggerLocalDataSource.removeLogs()
+    }
+
+    @Test
+    fun happyPath() {
+        ActivityScenario.launch(MainActivity::class.java)
+
+        // Check Buttons fragment screen is displayed
+        onView(withId(R.id.textView)).check(matches(isDisplayed()))
+
+        // Tap on Button 1
+        onView(withId(R.id.button1)).perform(click())
+
+        // Navigate to Logs screen
+        onView(withId(R.id.all_logs)).perform(click())
+
+        // Check Logs fragment screen is displayed
+        onView(withText(containsString("Interaction with 'Button 1'")))
+            .check(matches(isDisplayed()))
+    }
+}
---
+++ app/src/androidTest/java/com/nguyen/codelabhilt/CustomTestRunner.kt
@@ -0,0 +1,11 @@
+package com.nguyen.codelabhilt
+
+import android.app.Application
+import android.content.Context
+import androidx.test.runner.AndroidJUnitRunner
+import dagger.hilt.android.testing.HiltTestApplication
+
+class CustomTestRunner : AndroidJUnitRunner() {
+    override fun newApplication(cl: ClassLoader?, className: String?, context: Context?): Application =
+        super.newApplication(cl, HiltTestApplication::class.java.name, context)
+}
